b4cd5291e05a029316f7ad1d92594df621cf3a45
MustaqeemQ <mustaqeemquadry@outlook.com>
2022-09-30 08:32:28 +0100

    Changed build packages

--- LoginApp/Models/LoginDataModel.Designer.cs
+++ LoginApp/Models/LoginDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Reliance\source\repos\LoginApp\LoginApp\Models\LoginDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Reliance\source\repos\JustApp\LoginApp\Models\LoginDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
